597b32eff937d00984166b68eca66756b1fa6d7a
Eeernest <ernestgrabski16@wp.pl>
2026-06-09 10:47:10 +0200

    update(config): add admin credentials from .env file

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Config(BaseSettings):
   POSTGRES_URL: str
+
+  ADMIN_USERNAME: str
+
+  ADMIN_EMAIL: str
+
+  ADMIN_HASHED_PASSWORD: str
   
   model_config = SettingsConfigDict(
     env_file=".env",
