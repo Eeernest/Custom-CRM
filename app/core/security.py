cb2dbb084643257e0b8e4432d28c4ab38b1e1f64
Eeernest <ernestgrabski16@wp.pl>
2026-06-09 13:23:22 +0200

    feat(security): add Security class and method

---
+++ app/core/security.py
@@ -0,0 +1,10 @@
+from fastapi.concurrency import run_in_threadpool
+import jwt
+from pwdlib import PasswordHash
+
+class Security:
+  def __init__(self):
+    self.hasher = PasswordHash.recommended()
+
+  async def get_password_hash(self, password: str) -> str:
+    return await run_in_threadpool(self.hasher.hash, password)
