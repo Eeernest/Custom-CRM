8163e91b47df4227678ce6e6d3eca4a1909c251a
Eeernest <ernestgrabski16@wp.pl>
2026-06-11 12:07:00 +0200

    feat(alembic): add clients model by alembic migration

--- alembic/env.py
+++ alembic/env.py
@@ -10,6 +10,7 @@ from alembic import context
 from app.core.config import settings
 from app.db.database import Base
 from app.models.user_model import User
+from app.models.client_model import Client
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
